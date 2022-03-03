b1df3774ced7758b7619d9bf382ed86ccf0f79e1
vh-fsantos <vitor-hugo962@hotmail.com>
2022-03-03 00:52:21 -0300

    Finish Module 7

---
+++ api/routes/api/v1/clientes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const ClienteController = require("../../../controllers/ClienteController");
+const { LojaValidation } = require("../../../controllers/validacoes/lojaValidation");
+const { ClienteValidation } = require("../../../controllers/validacoes/clienteValidation");
+const Validation = require("express-validation");
+const auth = require("../../auth");
+
+const clienteController = new ClienteController();
+
+// ADMIN ROUTES
+router.get("/", auth.required, LojaValidation.admin, Validation(ClienteValidation.index), clienteController.index);
+// router.get("/search/:search/pedidos", auth.required, LojaValidation.admin, clienteController.searchPedido);
+router.get("/search/:search", auth.required, LojaValidation.admin, Validation(ClienteValidation.search), clienteController.search);
+
+router.get("/admin/:id", auth.required, LojaValidation.admin, Validation(ClienteValidation.showAdmin), clienteController.showAdmin);
+// router.get("/admin/:id/pedidos", auth.required, LojaValidation.admin, clienteController.showPedidosCliente);
+
+router.put("/admin/:id", auth.required, LojaValidation.admin, Validation(ClienteValidation.updateAdmin), clienteController.updateAdmin);
+
+// CLIENT ROUTES
+router.get("/:id", auth.required, Validation(ClienteValidation.show), clienteController.show);
+
+router.post("/", Validation(ClienteValidation.store), clienteController.store);
+router.put("/:id", auth.required, Validation(ClienteValidation.update), clienteController.update);
+router.delete("/:id", auth.required, clienteController.remove);
+
+module.exports = router;
